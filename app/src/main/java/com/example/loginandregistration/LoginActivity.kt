b7f506d2ff93c764ae40ffc43abcbb8d7de7fba1
Eisen <eisengong@gmail.com>
2023-11-09 09:19:29 -0800

    Initial commit

---
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.loginandregistration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loginandregistration.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loginandregistration
+
+class RegistrationActivity {
+
+}

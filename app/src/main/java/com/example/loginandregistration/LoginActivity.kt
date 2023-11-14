c5644b7ffc0597a54bdd392391d1b87fb63cf17e
Eisen <eisengong@gmail.com>
2023-11-14 10:07:18 -0800

    registrationActivity added

--- app/src/main/java/com/example/loginandregistration/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.loginandregistration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.loginandregistration.databinding.ActivityLoginBinding
@@ -8,11 +9,28 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    companion object {
+        //values to send Intents are called Extras and have the EXTRA_BLAH format
+        val EXTRA_USERNAME = "username"
+        val EXTRA_PASSWORD = "password"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textViewLoginSignUp.setOnClickListener{
+            //create an intent object with the current activity and the destination activity's class
+            val registrationIntent = Intent(this, RegistrationActivity::class.java)
+            //optionally, add information to send with the intent
+            //key-value pairs just like with bundles
+            registrationIntent.putExtra(EXTRA_USERNAME, binding.editTextTextLoginUsername.text.toString())
+            registrationIntent.putExtra(EXTRA_PASSWORD, binding.editTextTextPasswordLoginPassword.text.toString())
+            //launch the new activity using the intent
+            startActivity(registrationIntent)
+
+        }
 
     }
 }
--- app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -1,5 +1,25 @@
 package com.example.loginandregistration
 
-class RegistrationActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginandregistration.databinding.ActivityRegistrationBinding
 
+class RegistrationActivity: AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegistrationBinding
+    override fun onCreate(savedInstanceBundle: Bundle?) {
+        super.onCreate(savedInstanceBundle)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //retrieve any information from the Intent using the EXTRAs keys
+        val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME) ?: ""
+        val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD) ?: ""
+
+        //prefill the username and password fields
+        //for editTexts, you have to use the setText functions
+        binding.editTextTextRegistrationUsername.setText(username)
+        binding.editTextTextPasswordRegistrationPassword.setText(password)
+
+    }
 }

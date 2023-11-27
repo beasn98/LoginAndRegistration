6177329a876c44a4cd75ca868b9b6fe92413b562
Eisen <eisengong@gmail.com>
2023-11-27 09:40:50 -0800

    registrationActivity added, data validation incomplete

--- app/src/main/java/com/example/loginandregistration/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.loginandregistration
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.loginandregistration.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +18,16 @@ class LoginActivity : AppCompatActivity() {
         val EXTRA_PASSWORD = "password"
     }
 
+    val startRegistrationForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            // Handle the Intent to do whatever we need with the returned info
+            binding.editTextLoginUsername.setText(intent?.getStringExtra(EXTRA_USERNAME))
+            binding.editTextPasswordLoginPassword.setText(intent?.getStringExtra(EXTRA_PASSWORD))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -25,12 +38,12 @@ class LoginActivity : AppCompatActivity() {
             val registrationIntent = Intent(this, RegistrationActivity::class.java)
             //optionally, add information to send with the intent
             //key-value pairs just like with bundles
-            registrationIntent.putExtra(EXTRA_USERNAME, binding.editTextTextLoginUsername.text.toString())
-            registrationIntent.putExtra(EXTRA_PASSWORD, binding.editTextTextPasswordLoginPassword.text.toString())
+            registrationIntent.putExtra(EXTRA_USERNAME, binding.editTextLoginUsername.text.toString())
+            registrationIntent.putExtra(EXTRA_PASSWORD, binding.editTextPasswordLoginPassword.text.toString())
             //launch the new activity using the intent
-            startActivity(registrationIntent)
-
+            //startActivity(registrationIntent)
+            // Launch the activity for a result using the variable from the register for result contract above
+            startRegistrationForResult.launch(registrationIntent)
         }
-
     }
 }
--- app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -1,12 +1,15 @@
 package com.example.loginandregistration
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginandregistration.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity: AppCompatActivity() {
 
-    private lateinit var binding:ActivityRegistrationBinding
+    private lateinit var binding : ActivityRegistrationBinding
     override fun onCreate(savedInstanceBundle: Bundle?) {
         super.onCreate(savedInstanceBundle)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
@@ -18,8 +21,37 @@ class RegistrationActivity: AppCompatActivity() {
 
         //prefill the username and password fields
         //for editTexts, you have to use the setText functions
-        binding.editTextTextRegistrationUsername.setText(username)
-        binding.editTextTextPasswordRegistrationPassword.setText(password)
+        binding.editTextRegistrationUsername.setText(username)
+        binding.editTextPasswordRegistrationPassword.setText(password)
 
+        //register an account and send back the username and password to the login activity to prefill those fields
+        binding.buttonRegistrationRegister.setOnClickListener {
+            val password = binding.editTextPasswordRegistrationPassword.text.toString()
+            val confirm = binding.editTextPasswordRegistrationConfirmPassword.text.toString()
+            val username = binding.editTextRegistrationUsername.text.toString()
+            val email = binding.editTextEmailAddressRegistrationEmail.text.toString()
+            val name = binding.editTextRegistrationName.text.toString()
+
+            if (RegistrationUtil.validatePassword(password, confirm) &&
+                RegistrationUtil.validateUsername(username) &&
+                RegistrationUtil.validateEmail(email) &&
+                RegistrationUtil.validateName(name)) {
+                //apply lambda will call the functions inside it on the object that apply is called on
+                val resultIntent = Intent().apply {
+                    // apply { putExtra() } is doing the same thing as resultIntent.putExtra()
+                    putExtra(LoginActivity.EXTRA_USERNAME, binding.editTextRegistrationUsername.text.toString())
+                    putExtra(LoginActivity.EXTRA_PASSWORD, password)
+                }
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
+            }
+            else {
+                Toast.makeText(this, "Please fill out the fields correctly", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
+
+
 }
